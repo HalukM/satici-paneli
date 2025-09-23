ecfb6ddd9eaad650ed372e618be7c9565c37f603
HalukM <halukmorgul@hotmail.com>
2025-09-23 21:12:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000; 
 
 // Body parser limitini artırarak büyük base64 imaj verilerinin gönderilmesini sağlıyoruz.
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 
 
@@ -162,4 +162,5 @@ app.post('/shopify/products', async (req, res) => {
 app.listen(port, () => {
     console.log(`Sunucu http://localhost:${port} adresinde çalışıyor`);
 });
+
 
